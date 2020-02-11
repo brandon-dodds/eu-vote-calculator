722f7d138f97a6f094b4c1d1016d2ab668d8ecf3
Brandon-Lee Dodds <19696909@students.lincoln.ac.uk>
2020-02-11 20:18:14 +0000

    Changed the votecalc to have cool get; set; logic.

--- eu-vote-calculator/VoteCalc.cs
+++ eu-vote-calculator/VoteCalc.cs
@@ -7,7 +7,23 @@ namespace eu_vote_calculator
 
     class VoteCalc
     {
-        public List<Country> Countries { get; set; }
+        private List<Country> countries;
+        public IEnumerable<Country> yesVotes;
+        public List<Country> Countries
+        {
+            get
+            {
+                return countries;
+            }
+            set
+            {
+                countries = value;
+                yesVotes = from countries in Countries
+                           where countries.VoteChoice == 1
+                           select countries;
+
+            }
+        }
         /* This is an enum for the vote choice. This makes it a lot easier to manage if a country has abstained or not. And it is
          * a lot easier to convert between the types. It also makes abstain the default option. */
 
@@ -21,21 +37,12 @@ namespace eu_vote_calculator
             /* This is what is known as a LINQ Query! It's cool as it allows us to use an SQL like system to figure
              * who voted yes or not. We can use this to get a percentage for the vote mechanics! */
 
-            IEnumerable<Country> yesVotes =
-                from countries in Countries
-                where countries.VoteChoice == 1
-                select countries;
-
             return yesVotes.Count();
         }
 
         public double AmountOfYesPopulation()
         {
             double population = 0;
-            IEnumerable<Country> yesVotes =
-                from countries in Countries
-                where countries.VoteChoice == 1
-                select countries;
 
             for (int i = 0; i < yesVotes.Count(); i++)
             {
@@ -49,10 +56,6 @@ namespace eu_vote_calculator
         public bool QualifiedMajority()
         {
             double population = 0;
-            IEnumerable<Country> yesVotes =
-                from countries in Countries
-                where countries.VoteChoice == 1
-                select countries;
 
             for (int i = 0; i < yesVotes.Count(); i++)
             {
